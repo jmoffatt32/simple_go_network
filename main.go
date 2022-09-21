dc5c0f9d9aedf03e6671c7ca5bac4489e3c34b68
jmoffatt32 <jmoffatt32@gmail.com>
2022-09-21 10:47:12 -0400

    added error checking for command line arguments

--- main.go
+++ main.go
@@ -3,18 +3,35 @@ package main
 import (
 	"fmt"
 	"os"
+	"strconv"
 	"tcp-network/pkg/config"
 )
+
+func inputError() {
+	fmt.Println("Please provide a network ID: {1, 2, 3, 4}")
+}
 
 func main() {
 
 	// Take ID input from command line and initialize node address and delay
+	if len(os.Args) < 2 {
+		inputError()
+		return
+	}
+
+	// Set the network ID and check for errors taking the command line input
 	id := os.Args[1]
+	val, err := strconv.Atoi(os.Args[1])
+	if err != nil || val < 1 || val > 4 {
+		inputError()
+		return
+	}
+
+	// Initialize configuration variables
 	addrMap, delay := config.FetchConfig()
 	address := addrMap[id]
-
 	fmt.Println(address)
 	fmt.Println(delay)
 
-	// Implement packages to start a client and a package to start a server
+	// Implement package to start a client and a package to start a server...
 }
