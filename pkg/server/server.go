be83cc7f33827ad85249f487d34d29abc2e45f28
jmoffatt32 <jmoffatt32@gmail.com>
2022-09-25 23:42:11 -0400

    updated unicast receive funcitonality

--- pkg/server/server.go
+++ pkg/server/server.go
@@ -9,16 +9,14 @@ import (
 	"time"
 )
 
-type OutgoingMessage struct {
-	destination string
-	content     string
+type Message struct {
+	address string
+	content string
 }
 
 type Confirmed struct {
+	message   Message
 	direction string
-	address   string
-	content   string
-	status    bool
 	timestamp time.Time
 }
 
@@ -38,50 +36,50 @@ func parse_input(raw_input string) (string, string) {
 }
 
 func unicast_send(destination string, message string) {
-
 	c, err := net.Dial("tcp", destination)
 	check(err)
 	fmt.Fprintf(c, message+"\n")
 	c.Close()
-
 }
 
-func unicast_recieve(conn net.Conn, inbound chan Confirmed) {
+func unicast_recieve(c net.Conn, client net.Conn) {
 
-	src := conn.LocalAddr().String()
-	msg, err := bufio.NewReader(conn).ReadString('\n')
+	netData, err := bufio.NewReader(c).ReadString('\n')
 	check(err)
 
-	trimmed := strings.Trim(msg, "\n")
-	verified := Confirmed{"IN", src, trimmed, true, time.Now()}
-	inbound <- verified
+	temp := strings.TrimSpace(string(netData)) + "\n"
+	src := c.LocalAddr().String()
+	msg := Message{src, temp}
 
+	t := time.Now()
+	myTime := t.Format(time.RFC3339)
+	content := strings.Trim(msg.content, "\n")
+	client.Write([]byte(myTime + "---" + "Recieved: \"" + content + "\" to " + msg.address + "\n"))
+
+	c.Close()
 }
 
-func outgoing_routine(delays [2]int, outgoing_messages chan OutgoingMessage, confirmed chan Confirmed) {
-
+func incoming_routine(l net.Listener, client net.Conn) {
 	for {
-		var msg OutgoingMessage = <-outgoing_messages
-		delay := delays[0] + rand.Intn(delays[1]-delays[0])
-
-		time.Sleep(time.Duration(delay) * time.Millisecond)
-		unicast_send(msg.destination, msg.content)
-
-		trimmed := strings.Trim(msg.content, "\n")
-		verified := Confirmed{"OUT", msg.destination, trimmed, true, time.Now()}
-		confirmed <- verified
+		c, err := l.Accept()
+		check(err)
+		go unicast_recieve(c, client)
 	}
 }
 
-func incoming_routine(listener net.Listener, incoming chan Confirmed) {
-
+func outgoing_routine(delays [2]int, outgoing_messages chan Message, client net.Conn) {
 	for {
-		c, err := listener.Accept()
-		check(err)
+		var msg Message = <-outgoing_messages
+		delay := delays[0] + rand.Intn(delays[1]-delays[0])
 
-		unicast_recieve(c, incoming)
+		time.Sleep(time.Duration(delay) * time.Millisecond)
+		unicast_send(msg.address, msg.content)
 
-		c.Close()
+		sent := Confirmed{msg, "OUT", time.Now()}
+		t := sent.timestamp
+		myTime := t.Format(time.RFC3339)
+		content := strings.Trim(sent.message.content, "\n")
+		client.Write([]byte(myTime + "---" + "Sent: \"" + content + "\" to " + sent.message.address + "\n"))
 	}
 }
 
@@ -97,34 +95,9 @@ func Server(address string, addrMap map[string]string, delay [2]int) {
 	c, err := l.Accept()
 	check(err)
 
-	// Make channels to communicate with send and recieve goroutines
-	confirmed := make(chan Confirmed, 5)
-	outgoing := make(chan OutgoingMessage, 5)
-
-	// Start a go routine to handle sending outgoing messages. This accepts a channel to read
-	// messages to be sent as well as a channel to tell the server when a message has been sent.
-	go outgoing_routine(delay, outgoing, confirmed)
-
-	// NEED TO FINISH
-	// Start a go routine to handle incoming connections. This routine should implement unicast_receive
-	// to deleiver the message recieved from the source process
-	go incoming_routine(l, confirmed)
-
-	// Create a buffered channel to hold all the outputs to be sent
-	// read to the client at the end of each iteration.
-	go func() {
-		for element := range confirmed {
-			t := element.timestamp
-			myTime := t.Format(time.RFC3339)
-			if element.direction == "OUT" {
-				c.Write([]byte(myTime + "---" + "Sent: \"" + element.content + "\" to " + element.address + "\n"))
-			} else {
-				c.Write([]byte(myTime + "---" + "Received: \"" + element.content + "\" from " + element.address + "\n"))
-			}
-		}
-	}()
-	// Start loop to read the buffer for messages from the client to send
-	// This is the application layer
+	outgoing := make(chan Message, 5)
+	go incoming_routine(l, c)
+	go outgoing_routine(delay, outgoing, c)
 	for {
 		// Read the input from the client...
 		netData, err := bufio.NewReader(c).ReadString('\n')
@@ -135,17 +108,9 @@ func Server(address string, addrMap map[string]string, delay [2]int) {
 			return
 		}
 
-		// Parse users input to extract destination and content of the message to be sent...
-		dst_id, content := parse_input(netData)
-		// ... convert network ID to "host:port"...
-		dest := addrMap[dst_id]
-		// ... create a new Message and send it to the "outgoing_routine" via
-		// the outgoing channel.
-		var new_msg OutgoingMessage = OutgoingMessage{dest, content}
-		fmt.Println("Here it is: ")
-		fmt.Println(new_msg)
-		outgoing <- new_msg
-
+		dest, msg := parse_input(netData)
+		address := addrMap[dest]
+		outgoing <- Message{address, msg}
 	}
 
 }
