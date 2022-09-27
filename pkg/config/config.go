18f68e6085ab0d8cd9aee1a5241b051eb9b39edc
jmoffatt32 <jmoffatt32@gmail.com>
2022-09-26 22:48:39 -0400

    added config.go comments

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -8,18 +8,27 @@ import (
 	"strings"
 )
 
-func check(e error) {
-	if e != nil {
-		fmt.Println(e)
-		panic(e)
+// Simple error check function to catch errors and stop the program.
+// `check` will print the error message and then terminate the program.
+func check(err error) {
+	if err != nil {
+		fmt.Print(err)
+		return
 	}
 }
 
+// Configure network settings from "pkg/config/.config" file.
+// Opens file and reads scans line by line. To configure delay
+// settings and all possible addresses. Returns a map of network IDs
+// to network addresses and an array of min/max delay values.
 func FetchConfig() (map[string]string, [2]int) {
+
+	// Safely open the ".config" file
 	f, err := os.Open("pkg/config/.config")
 	check(err)
 	defer f.Close()
 
+	// Initialize all required variables
 	var delay [2]int
 	IDList := make([]string, 0)
 	IPList := make([]string, 0)
@@ -27,6 +36,7 @@ func FetchConfig() (map[string]string, [2]int) {
 	addrMap := make(map[string]string)
 
 	scanner := bufio.NewScanner(f)
+	// Begin scanning the file...
 	for scanner.Scan() {
 		x := strings.Split(string(scanner.Text()), " ")
 		for i, v := range x {
@@ -43,6 +53,7 @@ func FetchConfig() (map[string]string, [2]int) {
 	}
 
 	delay[0], err = strconv.Atoi(IDList[0])
+	check(err)
 	delay[1], err = strconv.Atoi(IPList[0])
 	check(err)
 	IDList = IDList[1:]
